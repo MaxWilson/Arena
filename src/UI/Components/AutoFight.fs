a506bb2f79041390c385a2803a7a08bf8bd95a25
Max Wilson <wilson.max@gmail.com>
2023-10-12 23:42:34 -0700

    Test + code changes to support issue #14, Perf too slow
    Instead of naively searching from 1..100, we do an open-ended search followed by a binary search, similar to a human being playing Guess Any Number.

--- src/UI/Components/AutoFight.fs
+++ src/UI/Components/AutoFight.fs
@@ -81,70 +81,119 @@ let specificFight db team1 team2 = async {
     return (cqrs.LogWithMessages() |> loggedOnly), victors
     }
 
+type DifficultyGuidance = TooLow | TooHigh | JustRight
+
+// find an inclusive range [lowest, highest] range of values that satisfy the given asynchronous evaluation function
+let findRange evaluate (hardCap: _ option) = async {
+    // first, look for a bound on the *upper* bound
+    let rec step1 n = async {
+        match! evaluate n with
+        | (TooLow | JustRight) when (hardCap.IsNone || n <= hardCap.Value) -> return! step1 (if n = 1 then 2 else n * 3 / 2) // about a 50% increase each time
+        | TooHigh | _ ->
+            let range = match hardCap with Some hardCap when n > hardCap -> (1, hardCap) | _ -> (1, n)
+            // then, use binary search to seek both the lower and upper bound
+            let rec binarySearch eval (minInclusive, maxInclusive) = async {
+                let mid = (minInclusive + maxInclusive) / 2
+                if minInclusive >= maxInclusive then return minInclusive // if we've narrowed it down to a single value, accept it as the best we can do
+                else
+                    match! eval mid with
+                    | TooLow when minInclusive + 1 = maxInclusive -> return maxInclusive // special case when min and max are adjacent--no more recursion
+                    | TooHigh when minInclusive + 1 = maxInclusive -> return minInclusive // special case when min and max are adjacent--no more recursion
+                    | TooLow -> return! binarySearch eval (mid, maxInclusive)
+                    | TooHigh -> return! binarySearch eval (minInclusive, mid)
+                    | JustRight -> return mid
+                }
+            let rec findLowerBound n = async {
+                let! evalN = evaluate n
+                let! evalJustBelow = evaluate (n-1)
+                return
+                    match evalN, evalJustBelow with
+                    | JustRight, TooLow -> JustRight
+                    | TooLow, _ -> TooLow
+                    | TooHigh, _ -> TooHigh
+                    | JustRight, JustRight -> TooHigh // go lower until you find a JustRight/TooLow pair.
+                    | JustRight, TooHigh -> TooHigh // Note: JustRight/TooHigh doesn't make sense but could happen anyway if eval is noisy.
+                }
+            let rec findUpperBound n = async {
+                let! evalN = evaluate n
+                let! evalJustAbove = evaluate (n+1)
+                return
+                    match evalN, evalJustAbove with
+                    | JustRight, TooHigh -> JustRight
+                    | TooLow, _ -> TooLow
+                    | TooHigh, _ -> TooHigh
+                    | JustRight, JustRight -> TooLow // go higher until you find a JustRight/TooHigh pair.
+                    | JustRight, TooLow -> TooLow // Note: JustRight/TooLow doesn't make sense but could happen anyway if eval is noisy.
+                }
+            let! lower = binarySearch findLowerBound range
+            let! upper = binarySearch findUpperBound range
+            return lower, upper
+        }
+    return! step1 1
+    }
+
 let calibrate inform (db: Map<_,Creature>) (team1: TeamSetup) (center: Coords, radius: Distance option, enemyType, minbound, maxbound, defeatCriteria) = async {
     let enemyStats = db[enemyType]
-    let runForN n = async {
+    let runForN n run = async {
+        inform $"Evaluating vs. {enemyStats.Quantify n} ({run}/10)"
         do! Async.Sleep 0 // yield the JS runtime  in case UI updates need to be processed
         let combat = createCombat db team1 [{ members = [n, enemyType ]; center = center; radius = radius }] // instantiate. TODO: instantiate at specific positions, as soon as monsters have positions.
         let cqrs = CQRS.CQRS.create(combat, CombatAtom.update)
         return cqrs, fight cqrs
         }
-    let mutable results: Map<_,int*AugmentedCombatLog> = Map.empty
     let get n = async {
         inform $"Evaluating vs. {enemyStats.Quantify n}"
-        if results.ContainsKey n then return results[n]
-        else
-            let! runs =
-                [
-                    for run in 1..10 do
-                        runForN n
-                    ]
-                |> Async.Parallel
-            let sampleLog: AugmentedCombatLog = (runs |> Array.last |> fst).LogWithMessages()
-            let victoryMetric : CQRS.CQRS<_, AugmentedCombat> * {| victors: int list |} -> int =
-                match defeatCriteria with
-                | TPK -> function (_, v) when v.victors = [1] -> 1 | otherwise -> 0
-                | OneCasualty ->
-                    fun (cqrs, v) ->
-                        // in this case, TeamA is very casualty-averse. Defeat is taking even one casualty (dead or unconscious).
-                        if cqrs.State.combat.combatants.Values |> Seq.exists (fun c ->
-                            c.team = 1 && c.isAny[Dead; Unconscious]) then
-                            0
-                        else 1
-                | HalfCasualties ->
-                    fun (cqrs, v) ->
-                        // in this case, TeamA is somewhat casualty-averse. Defeat is a pyrrhic victory where at least half the team dies.
-                        let friendlies = cqrs.State.combat.combatants.Values |> Seq.filter (fun c -> c.team = 1)
-                        let deadFriendlies = friendlies |> Seq.filter (fun c -> c.isAny[Dead; Unconscious])
-                        if deadFriendlies |> Seq.length >= ((friendlies |> Seq.length) / 2) then
-                            0
-                        else 1
+        let! runs = async {
+            let mutable finished = []
+            for run in 1..10 do
+                let! r = runForN n run
+                finished <- r :: finished
+            return finished
+            }
+        let victoryMetric : CQRS.CQRS<_, AugmentedCombat> * {| victors: int list |} -> int =
+            match defeatCriteria with
+            | TPK -> function (_, v) when v.victors = [1] -> 1 | otherwise -> 0
+            | OneCasualty ->
+                fun (cqrs, v) ->
+                    // in this case, TeamA is very casualty-averse. Defeat is taking even one casualty (dead or unconscious).
+                    if cqrs.State.combat.combatants.Values |> Seq.exists (fun c ->
+                        c.team = 1 && c.isAny[Dead; Unconscious]) then
+                        0
+                    else 1
+            | HalfCasualties ->
+                fun (cqrs, v) ->
+                    // in this case, TeamA is somewhat casualty-averse. Defeat is a pyrrhic victory where at least half the team dies.
+                    let friendlies = cqrs.State.combat.combatants.Values |> Seq.filter (fun c -> c.team = 1)
+                    let deadFriendlies = friendlies |> Seq.filter (fun c -> c.isAny[Dead; Unconscious])
+                    if deadFriendlies |> Seq.length >= ((friendlies |> Seq.length) / 2) then
+                        0
+                    else 1
 
-            let victories = runs |> Array.sumBy victoryMetric
-            results <- results |> Map.add n (victories, sampleLog |> List.filter (function Some (Unlogged _), _ -> false | _ -> true))
-            return results[n]
+        let victories = runs |> List.sumBy victoryMetric
+        return victories
         }
 
-    // crude and naive model: search from 1 to 100, but quit early when we fall to 0% victory
-    let! upToOneHundred =
-        let rec loop n = async {
-            let! (victories, log) as result = get n
-            if victories > 0 && n <= 100 then
-                let! looped = loop (n+1)
-                return (n, result)::looped
-            else return []
-            }
-        loop 1
-    let inbounds (n, result) =
-        betweenInclusive (minbound * 10. |> int) (maxbound * 10. |> int) (result |> fst)
-
-    match upToOneHundred |> List.filter inbounds with
-    | [] ->
-        return None, None, None
-    | inbounds ->
-        let min, _ = inbounds |> List.minBy fst
-        let max, (_, sampleFight) = inbounds |> List.maxBy fst
-        return Some min, Some max, Some (loggedOnly sampleFight)
+    let mutable resultsCache = Map.empty
+    let eval n = async {
+        if resultsCache.ContainsKey n then return resultsCache[n]
+        else
+            let! victories= get n
+            let v =
+                if float victories / 100. < minbound then TooHigh // make it easier, and hard cap at 100 monsters to prevent nontermination
+                elif float victories / 100. > maxbound then TooLow // make it harder
+                else JustRight
+            resultsCache <- resultsCache |> Map.add n v
+            return resultsCache[n]
+        }
+    match! findRange eval (Some 100) with
+    | min, max ->
+        let fightFor n =
+            let combat = createCombat db team1 [{ members = [n, enemyType ]; center = center; radius = radius }] // instantiate. TODO: instantiate at specific positions, as soon as monsters have positions.
+            let cqrs = CQRS.CQRS.create(combat, CombatAtom.update)
+            fight cqrs |> ignore
+            let sampleFight = cqrs.LogWithMessages()
+            loggedOnly sampleFight
+        return Some min, Some max, Some (fightFor max)
     }
 
 let beginFights (model: Model) dispatch =
@@ -169,7 +218,7 @@ let beginFights (model: Model) dispatch =
                     let max = (defaultArg max 90 |> float) / 100.
                     match! calibrate (Some >> InProgress >> Fighting >> dispatch) model.database.catalog
                             model.fightSetup.sideA
-                            (sideB.center, sideB.radius, name, min, max, defeatCriteria) with
+                            (sideB.center, sideB.radius, name, float min / 100., float max / 100., defeatCriteria) with
                     | minQuantity, maxQuantity, Some sampleMaxFight ->
                         Completed(model.fightSetup, CalibratedResult(minQuantity, maxQuantity, sampleMaxFight)) |> Fighting |> dispatch
                     | v ->
--- test/UI.Logic.Tests.fs
+++ test/UI.Logic.Tests.fs
@@ -2,31 +2,16 @@ module UI.Logic.Tests
 open Common
 open Expecto
 open Packrat
-open Hedgehog
 open Domain
 open Domain.Parser
 open Domain.Random
 open Domain.Random.Parser
+open UI.Components.AutoFight
 open Swensen.Unquote
 
-let findRange bound1 bound2 evaluate =
-    let lower, upper = min bound1 bound2, max bound1 bound2
-    let (|TooLow|JustRight|TooHigh|) n =
-        let v = evaluate n
-        if v > upper then TooHigh // if victory percentage is too high, we need to increase n
-        elif v < lower then TooLow // if victory percentage is too low, we need to decrease n
-        else JustRight
-    // first, look for a bound on the *upper* bound
-    let rec step1 n =
-        match n with
-        | TooLow | JustRight -> step1 (n * 2)
-        | TooHigh ->
-            let range = (1, n)
-            notImpl "now we just do a binary search on [1, n] for both upper bound and lower bound"
-    step1 1
 [<Tests>]
 let Tests() = (testLabel "Unit") <| testList "UI" [
-    testCase "Spot-check AutoFight.calibrate using mocked evaluation" <| fun () ->
+    testCase "Spot-check AutoFight calibrate using mocked evaluation" <| fun () ->
         let vals = [|
             for n in 0..100 do
                 if n < 11 then 100
@@ -39,13 +24,14 @@ let Tests() = (testLabel "Unit") <| testList "UI" [
                 else 0
             |]
         let getVal n = vals.[n]
-        test <@ findRange 50 90 getVal = (11, 30) @>
-    testCase "Placeholder for property testing AutoFight.calibrate" <| fun () ->
-        // not sure if this is really necessary
-        property {
-            notImpl "Given a list of times... what property should be true?"
-            let! xs = Range.constant 0 1000 |> Gen.int32 |> Gen.list (Range.linear 0 100)
-            return List.rev (List.rev xs) = xs
-        }
-        |> Property.checkBool
+        let eval n = async {
+            let v = getVal n
+            return
+                if v > 90 then TooLow // if victory percentage is too high, we need to increase n
+                elif v < 50 then TooHigh // if victory percentage is too low, we need to decrease n
+                else JustRight
+            }
+        test <@ (findRange eval None |> Async.RunSynchronously) = (11, 30) @>
+        test <@ (findRange eval (Some 20) |> Async.RunSynchronously) = (11, 20) @>
+        test <@ (findRange eval (Some 5) |> Async.RunSynchronously) = (5, 5) @>
     ]
